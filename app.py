49297fd51fbc4d48e795e0bd85129245c0897a73
Sandesh Devaraju <scouredimage@gmail.com>
2020-04-10 01:26:40 -0700

    up

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route("/hello/<name>")
 def hello(name):
     return "Hello %s!" % name
+
+@app.route("/goodbye/<name>")
+def goodbye(name):
+    return "Goodbye %s!" % name
+
+@app.route("/howdy/<name>")
+def howdy(name):
+    raise Exception('how do?')
